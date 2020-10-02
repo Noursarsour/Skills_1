cebf0e465fe876ca9b9de30b68ebe2c9d88f253c
Ashley Trinh <ashley@hackbrightacademy.com>
2020-10-01 17:54:59 -0700

    No tests for now

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""tests/conftest.py"""
-
-
-def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    def noop(*args):
-        return
-
-    terminalreporter.short_test_summary = noop
